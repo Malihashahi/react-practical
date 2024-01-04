851b5c3d3bc0c29360f0213f6a1408ea099a85c4
maliha shahi <maliha.shahi.03@gmail.com>
2024-01-04 22:06:31 +0330

    add new componenet for adding user in page or react

---
+++ src/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { MainContext } from './contexts/MainContext';
+import { Link} from 'react-router-dom';
+import style from './style.module.css';
+
+const Sidebar = ()=>{
+
+    const {showMenu,setShowMenu} = useContext(MainContext)
+
+    return (
+        <div className={`${style.sidebar_section} bg-secondary`} style={showMenu ? {right:0} : {}}>
+            <ul className={`${style.sidebar_list} m-0 p-0`}>
+                <li className={style.sidebar_avatar}>
+                    <img src="/assets/images/user2.jpg" alt="" />
+                </li>
+                <li>
+                    <Link to="/user">کاربران</Link>
+                </li>
+                <li>
+                    <Link to="/post">پست ها</Link>
+                </li>
+                <li>
+                    <Link to="/gallery">گالری</Link>
+                </li>
+                <li>
+                    <Link to="/todo">کارها</Link>
+                </li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default Sidebar;
---
+++ src/users/Users.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import style from '../style.module.css'
+
+const Users = ()=>{
+
+    return (
+        <div className={`${style.item_content} mt-5 p-4 container-fluid`}>
+            <h4 className="text-center">مدیریت کاربران</h4>
+            <div className="row my-2 mb-4 justify-content-between w-100 mx-0">
+                <div className="form-group col-10 col-md-6 col-lg-4">
+                    <input type="text" className="form-control shadow" placeholder="جستجو"/>
+                </div>
+                <div className="col-2 text-start px-0">
+                    <Link to="/user/add">
+                        <button className="btn btn-success">
+                            <i className="fas fa-plus text-light"></i>
+                        </button>
+                    </Link>
+                </div>
+            </div>
+            <table className="table bg-light shadow">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>نام</th>
+                        <th>نام کاربری</th>
+                        <th>ایمیل</th>
+                        <th>عملیات</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>maliha</td>
+                        <td>shahi</td>
+                        <td>malihashahi@gmail.com</td>
+                        <td>
+                            <Link to="/user/add/2">
+                                <i className="fas fa-edit text-warning mx-2 pointer"></i>
+                            </Link>
+                            <i className="fas fa-trash text-danger mx-2 pointer"></i>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+
+}
+
+export default Users;
