b5efa807877d8bec86307ea2413a78a1ecdfb9ca
maliha shahi <maliha.shahi.03@gmail.com>
2024-01-26 23:13:48 +0330

    previous state at react

--- src/posts/Counter.jsx
+++ src/posts/Counter.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-const Counter =(MainComponent,number)=>{
-  const NewComponent=(props)=>{
-    const [count,setCount]= useState(0);
-    const handleIncreaseCount=()=>{
-      setCount(count+number)
-    }
-return(
-  <MainComponent {...props} count={count} handleIncreaseCount={handleIncreaseCount} />
-)
-  }
-  return NewComponent;
+const Counter = () => {
+const [count,setCount]= useState(0);
+const handleIncreaseCount=(num)=>{
+ for(let index=0; index<num;index++){
+  setCount((prevCount)=>{
+   return prevCount +1;
+  });
 
+ }
 }
+
+  return (
+    <div className='text-center my-3'>
+      <button className='btn btn-success' onClick={()=>handleIncreaseCount(5)}>count :{count}</button>
+    </div>
+  );
+}
+
 export default Counter;
